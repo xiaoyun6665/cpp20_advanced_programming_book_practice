c4a6127e4c0efac43742ba44eb45b5d4c8100d1e
xiaoyun6665 <LxY07598426*>
2022-11-06 23:25:43 +0800

    feat:chapter_one 3

--- chapter_one/2/src/main.cpp
+++ chapter_one/2/src/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <format>
+#include <array>
 using namespace std;
 
 import Hr;
 
 int main(){
-    Hr::Employee employee {'J', 'D', 42, 8000, Hr::Title::ENGINEER};
-    cout<<format(" {} \n {} \n {} \n {} \n", employee.firstname, employee.lastname, employee.employeeNumber, employee.salary);
+    Hr::Employee employee1 {'J', 'D', 42, 8000, Hr::Title::ENGINEER};
+    Hr::Employee employee2 {'T', 'D', 42, 8000, Hr::Title::ENGINEER};
+    Hr::Employee employee3 {'Y', 'D', 42, 8000, Hr::Title::ENGINEER};
+    array<Hr::Employee, 3> lists =  {employee1, employee2, employee3};
+    for(auto i = 0; i < lists.size(); i++){
+        cout<<format(" {} \n {} \n {} \n {} \n", lists[i].firstname, lists[i].lastname, lists[i].employeeNumber, lists[i].salary);
+    }
     return 0;
 }
